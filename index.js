6fcef4230a771004b4b336583b46d371e18d2fa3
duongBinhMinh1003 <duongbinhminh10032004@gmail.com>
2023-10-24 15:15:09 +0700

    bai tap js 9-10

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function themNv() {
     var _gioLam = document.getElementById("gioLam").value * 1;
     
     var nv = new nhanVien(_taiKhoan, _name, _email, _matKhau, _ngay, _luong, _chucVu, _gioLam);
-    var isValid =kiemTrung(dsnv,nv.taiKhoan) & kiemTraDoDai(nv.taiKhoan, "tbTKNV", 4, 6)  ;
+    var isValid =kiemTrung(dsnv,nv.taiKhoan) && kiemTraDoDai(nv.taiKhoan, "tbTKNV", 4, 6)  ;
     isValid = isValid & kiemTraName(nv.name, "tbTen");
     isValid = isValid & kiemEmail(nv.email);
     isValid = isValid & kiemTraPassword(nv.matKhau);
